914baf1cc566b7737d79a6c5a02cbd8fccab1bfa
Eduardo Juarez <gjuarez@finanzas.df.gob.mx>
2017-12-19 17:41:09 -0600

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="login">
+      <h3>¡Inicia Sesión!</h3>
+      <input type="text" placeholder="Email"/><br>
+      <input type="password" placeholder="Password"/><br>
+      <button>Conectate</button>
+      <p>¿Aun no tienes una cuenta? ¡Puedes <router-link to="/sign-up">crear alguna!</router-link></p>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'login',
+      data: function () {
+        return {}
+      },
+      methods: {}
+    }
+</script>
+
+<style scoped>
+  .login {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="sign-up">
+      <h3>¡Registrate!</h3>
+      <input type="text" placeholder="Email"/><br>
+      <input type="password" placeholder="Password"/><br>
+      <button>Registrarse</button>
+      <span>o vuelve al <router-link to="/login">Inicio de Sesión.</router-link></span>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'signUp',
+      data: function () {
+        return {}
+      },
+      methods: {}
+    }
+</script>
+
+<style scoped>
+  .sign-up {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 10px;
+    width: 10%;
+    cursor: pointer;
+  }
+  span {
+    display: block;
+    margin-top: 20px;
+    font-size: 11px;
+  }
+</style>
